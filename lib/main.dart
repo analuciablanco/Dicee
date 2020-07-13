e17c6776668abec1606f7e7650511f5bbbf451ca
Ana Lucia Blanco <analuciablanco@outlook.com>
2020-07-13 15:05:19 -0700

    buttons and variables added

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,31 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var leftDiceNumber = 3;
+    var rightDiceNumber = 5;
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Left button pressed');
+              },
+              padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right button pressed');
+              },
+              padding: const EdgeInsets.fromLTRB(8.0, 16.0, 16.0, 16.0),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
